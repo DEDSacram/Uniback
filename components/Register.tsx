25fd86a737f3aaa98be7dd264998436eebc34394
HraZak <hrazak.hz@gmail.com>
2022-02-25 22:34:00 +0100

    Smazání MD5

--- components/Register.tsx
+++ components/Register.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { BsFillXCircleFill } from "react-icons/bs";
+import React from 'react';
+import { BsFillXCircleFill } from 'react-icons/bs';
 import { Formik, Field, Form, FormikHelpers } from 'formik';
-import styled from 'styled-components'
-import {Md5} from 'ts-md5/dist/md5';
+import styled from 'styled-components';
 
 interface Values {
     username: string;
@@ -11,102 +10,105 @@ interface Values {
     email: string;
 }
 // reactstrap components
-import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
+import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap';
 
 function Example() {
-  const [modalOpen, setModalOpen] = React.useState(false);
+    const [modalOpen, setModalOpen] = React.useState(false);
 
-  const IconButton = styled.button`
-    background-color: Transparent;
-    background-repeat:no-repeat;
-    border: none;
-    cursor:pointer;
-    overflow: hidden;
-    outline:none;
-`
-  return (
-    <>
-      <Button
-        color="primary"
-        type="button"
-        onClick={() => setModalOpen(!modalOpen)}
-      >
-      Registrovat
-      </Button>
+    const IconButton = styled.button`
+        background-color: Transparent;
+        background-repeat: no-repeat;
+        border: none;
+        cursor: pointer;
+        overflow: hidden;
+        outline: none;
+    `;
+    return (
+        <>
+            <Button color="primary" type="button" onClick={() => setModalOpen(!modalOpen)}>
+                Registrovat
+            </Button>
 
+            <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
+                <div className=" modal-header">
+                    <h1 className=" modal-title" id="exampleModalLabel">
+                        Registrace
+                    </h1>
+                    <IconButton type="button" onClick={() => setModalOpen(!modalOpen)}>
+                        {' '}
+                        <BsFillXCircleFill onClick={() => setModalOpen(!modalOpen)} />{' '}
+                    </IconButton>
+                </div>
 
-      <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
-        <div className=" modal-header">
-          <h1 className=" modal-title" id="exampleModalLabel">
-            Registrace
-          </h1>
-          <IconButton
-            type="button"
-            onClick={() => setModalOpen(!modalOpen)}
-          > <BsFillXCircleFill   onClick={() => setModalOpen(!modalOpen)}/> </IconButton>
-        </div>
+                <ModalBody>
+                    <Formik
+                        initialValues={{
+                            username: '',
+                            password: '',
+                            confirmpassword: '',
+                            email: '',
+                        }}
+                        onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+                            setTimeout(() => {
+                                alert(JSON.stringify(values, null, 2));
+                                setSubmitting(false);
+                            }, 500);
+                        }}
+                    >
+                        <Form>
+                            <div className="mb-3">
+                                <Field
+                                    className="form-control"
+                                    id="username"
+                                    name="username"
+                                    placeholder="Přezdívka"
+                                    aria-describedby="usernameHelp"
+                                    required
+                                />
+                            </div>
 
+                            <div className="mb-3">
+                                <Field
+                                    className="form-control"
+                                    id="password"
+                                    name="password"
+                                    placeholder="Heslo"
+                                    type="password"
+                                    required
+                                />
+                            </div>
 
-        <ModalBody>
+                            <div className="mb-3">
+                                <Field
+                                    className="form-control"
+                                    id="confirmpassword"
+                                    name="confirmpassword"
+                                    placeholder="Potvrzení hesla"
+                                    type="password"
+                                    required
+                                />
+                            </div>
 
-        <Formik
-          initialValues={{
-            username: '',
-            password: '',
-            confirmpassword: '',
-            email: '',
-          }}
+                            <div className="mb-3">
+                                <Field
+                                    className="form-control"
+                                    id="email"
+                                    name="email"
+                                    placeholder="Email (Optional)"
+                                />
+                            </div>
 
-          onSubmit={(
-            values: Values,
-            { setSubmitting }: FormikHelpers<Values>
-          ) => {
-            alert(Md5.hashStr(values.password))
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 500);
+                            <button type="submit" className="btn btn-primary">
+                                Registrovat
+                            </button>
+                        </Form>
+                    </Formik>
+                </ModalBody>
 
-
-
-
-
-
-          }}
-
-        >
-          <Form>
-            <div className="mb-3">
-              <Field className="form-control" id="username" name="username" placeholder="Přezdívka" aria-describedby="usernameHelp" required/>
-            </div>
-  
-            <div className="mb-3">
-              <Field className="form-control" id="password" name="password" placeholder="Heslo" type="password" required/>
-            </div>
-
-            <div className="mb-3">
-              <Field className="form-control" id="confirmpassword" name="confirmpassword" placeholder="Potvrzení hesla" type="password" required/>
-            </div>
-
-            <div className="mb-3">
-              <Field className="form-control" id="email" name="email" placeholder="Email (Optional)"/>
-            </div>
-          
-            <button type="submit" className="btn btn-primary">Registrovat</button>
-            
-          </Form>
-        </Formik>
-
-        </ModalBody>
-
-        <ModalFooter>
-    
-        
-        </ModalFooter>
-
-      </Modal>
-    </>
-  );
+                <ModalFooter></ModalFooter>
+            </Modal>
+        </>
+    );
 }
 
 export default Example;
