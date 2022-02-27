05fa3ec153d325ccfc2a3ab8c4bd84f70fda9fe4
HraZak <hrazak.hz@gmail.com>
2022-02-27 09:53:57 +0100

    Hash funkce

---
+++ components/funkce.ts
@@ -0,0 +1,37 @@
+import { createHash, randomBytes } from 'crypto';
+
+const twoOrMoreWhiteSpace = /\s{2,}/gm;
+
+export const sanitizeString = (text: string): string => {
+    return text.normalize().trim().replace(twoOrMoreWhiteSpace, ' ');
+};
+
+const hashPassword = (password: string, salt: string): string => {
+    const hash = createHash('sha512');
+    hash.update(password);
+    hash.update(salt);
+    return hash.digest('hex');
+};
+
+export const newHashPassword = (password: string) => {
+    const salt = randomBytes(256).toString('hex');
+    const hash = hashPassword(password, salt);
+    return {
+        hash: hash,
+        salt: salt,
+    };
+};
+
+export const checkPassword = (password: string, hash: string, salt: string): boolean => {
+    const passwordHash = hashPassword(password, salt);
+    return hash == passwordHash ? true : false;
+};
+
+// export const hashPassword = (password: string): string => {
+//     pbkdf2(password, 'salt', 100000, 64, 'sha512', (err, derivedKey) => {
+//         if (err) throw err;
+//         console.log(derivedKey.toString('hex'));
+//     });
+
+//     return '';
+// };
