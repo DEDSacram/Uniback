b8a11ac3b01167f05fb84ac429027628129c0ec8
DEDSacram <80846885+DEDSacram@users.noreply.github.com>
2022-03-03 20:34:20 +0100

    Add files via upload

---
+++ components/Buttom.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+const Button = styled.button`
+color: #fff;
+background-color: #0d6efd;
+border-color: #0d6efd;
+border: 1px solid transparent;
+padding: 0.375rem 0.75rem;
+font-size: 1rem;
+transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+`
+export default Button // :)import styled
